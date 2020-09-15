717bb2414cbe35bbd2bc31f7fd812723779c43bf
Sohail Karim <sohailkarim66@gmail.com>
2020-09-16 02:22:53 +0500

    integrate devise and basic configuration of databse and set the paths for the users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "visitors#index"
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
